1d99d7db7187abd93d81345ed18635827ddfcc0e
System Administrator <admin@MngEnvMCAP553100.onmicrosoft.com>
2025-04-04 00:31:19 +0000

    Updated updatefromgit.ps1

--- scripts/updatefromgit.ps1
+++ scripts/updatefromgit.ps1
@@ -81,45 +81,44 @@ function GetErrorResponse($exception) {
     return $errorResponse
 }
 
-try {
-    Write-Host "Setting Fabric Header"
-    SetFabricHeaders
+Write-Host "Setting Fabric Header"
+SetFabricHeaders
 
-    $workspace = GetWorkspaceByName $workspaceName 
-    
-    # Verify the existence of the requested workspace
-	if(!$workspace) {
-	  Write-Host "A workspace with the requested name was not found." -ForegroundColor Red
-	  return
-	}
-	
-    # Get Status
-    Write-Host "Calling GET Status REST API to construct the request body for UpdateFromGit REST API."
+$workspace = GetWorkspaceByName $workspaceName 
 
-    $gitStatusUrl = "{0}/workspaces/{1}/git/status" -f $global:baseUrl, $workspace.Id
-    $gitStatusResponse = Invoke-RestMethod -Headers $global:fabricHeaders -Uri $gitStatusUrl -Method GET
-
-    # Update from Git
-    Write-Host "Updating the workspace '$workspaceName' from Git."
-
-    $updateFromGitUrl = "{0}/workspaces/{1}/git/updateFromGit" -f $global:baseUrl, $workspace.Id
-
-    $updateFromGitBody = @{ 
-        remoteCommitHash = $gitStatusResponse.RemoteCommitHash
-		workspaceHead = $gitStatusResponse.WorkspaceHead
-        options = @{
-            # Allows overwriting existing items if needed
-            allowOverrideItems = $TRUE
-        }
-    } | ConvertTo-Json
-
-    $updateFromGitResponse = Invoke-WebRequest -Headers $global:fabricHeaders -Uri $updateFromGitUrl -Method POST -Body $updateFromGitBody
-
-    $operationId = $updateFromGitResponse.Headers['x-ms-operation-id']
-    $retryAfter = $updateFromGitResponse.Headers['Retry-After']
-    Write-Host "Long Running Operation ID: '$operationId' has been scheduled for updating the workspace '$workspaceName' from Git with a retry-after time of '$retryAfter' seconds." -ForegroundColor Green
-
-} catch {
-    $errorResponse = GetErrorResponse($_.Exception)
-    Write-Host "Failed to update the workspace '$workspaceName' from Git. Error reponse: $errorResponse" -ForegroundColor Red
+# Verify the existence of the requested workspace
+if(!$workspace) {
+    Write-Host "A workspace with the requested name was not found." -ForegroundColor Red
+    return
 }
+
+# Get Status
+Write-Host "Calling GET Status REST API to construct the request body for UpdateFromGit REST API."
+
+$gitStatusUrl = "{0}/workspaces/{1}/git/status" -f $global:baseUrl, $workspace.Id
+$gitStatusResponse = Invoke-RestMethod -Headers $global:fabricHeaders -Uri $gitStatusUrl -Method GET
+
+# Update from Git
+Write-Host "Updating the workspace '$workspaceName' from Git."
+
+$updateFromGitUrl = "{0}/workspaces/{1}/git/updateFromGit" -f $global:baseUrl, $workspace.Id
+
+$updateFromGitBody = @{ 
+    remoteCommitHash = $gitStatusResponse.RemoteCommitHash
+    workspaceHead = $gitStatusResponse.WorkspaceHead
+    options = @{
+        # Allows overwriting existing items if needed
+        allowOverrideItems = $TRUE
+    }
+} | ConvertTo-Json
+
+$updateFromGitResponse = Invoke-WebRequest -Headers $global:fabricHeaders -Uri $updateFromGitUrl -Method POST -Body $updateFromGitBody
+
+$operationId = $updateFromGitResponse.Headers['x-ms-operation-id']
+$retryAfter = $updateFromGitResponse.Headers['Retry-After']
+Write-Host "Long Running Operation ID: '$operationId' has been scheduled for updating the workspace '$workspaceName' from Git with a retry-after time of '$retryAfter' seconds." -ForegroundColor Green
+
+#} catch {
+#    $errorResponse = GetErrorResponse($_.Exception)
+#    Write-Host "Failed to update the workspace '$workspaceName' from Git. Error reponse: $errorResponse" -ForegroundColor Red
+#}
