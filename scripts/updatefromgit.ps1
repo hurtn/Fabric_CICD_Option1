18276dcad49080a76ddad32047700d1333d42b7d
System Administrator <admin@MngEnvMCAP553100.onmicrosoft.com>
2025-04-04 00:42:35 +0000

    Updated updatefromgit.ps1

--- scripts/updatefromgit.ps1
+++ scripts/updatefromgit.ps1
@@ -16,6 +16,10 @@
 # Parameters - fill these in before running the script!
 # =====================================================
 
+if(-not (Get-Module Az -ListAvailable)) { 
+    Install-Module Az -Scope CurrentUser -Force
+}
+
 param(
 [string]$workspaceName,
 [string]$pfabricToken
