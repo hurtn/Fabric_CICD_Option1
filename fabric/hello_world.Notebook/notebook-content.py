922e4d17ff4be67bf27b8000fd6c49e2da49f472
System Administrator <admin@MngEnvMCAP553100.onmicrosoft.com>
2025-04-16 10:54:03 +0000

    Initial

--- fabric/hello_world.Notebook/notebook-content.py
+++ fabric/hello_world.Notebook/notebook-content.py
@@ -8,12 +8,12 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "aba5e80f-9ae7-4be7-be3b-684a4ea9cc54",
+# META       "default_lakehouse": "dc804356-0dcd-474f-aff6-88ad1ce8a202",
 # META       "default_lakehouse_name": "bronze",
-# META       "default_lakehouse_workspace_id": "2ba131da-8f02-409a-a2a4-c9db35bd5ae8",
+# META       "default_lakehouse_workspace_id": "af063d47-af62-4bba-837a-23d47fe0b935",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "aba5e80f-9ae7-4be7-be3b-684a4ea9cc54"
+# META           "id": "dc804356-0dcd-474f-aff6-88ad1ce8a202"
 # META         }
 # META       ]
 # META     }
